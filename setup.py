7f2847503fb890933c4aa9f18e311d25ed7f124f
仌 POURYA <ogpourya@gmail.com>
2025-12-29 14:51:14 +0000

    build: bump version to 1.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="git-bard",
-    version="0.1.0",
+    version="1.0.0",
     py_modules=["git_bard"],
     install_requires=[
         "google-genai",
